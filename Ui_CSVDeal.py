3cfabea26cb217c9455d68d5b0ae14f354d379ea
Ki J <xx517230@gmail.com>
2024-08-27 10:52:40 +0800

    20240827

--- Ui_CSVDeal.py
+++ Ui_CSVDeal.py
@@ -14,19 +14,19 @@ from PySide2.QtWidgets import *
 
 import res_rc
 
-class Ui_Form(object):
-    def setupUi(self, Form):
-        if not Form.objectName():
-            Form.setObjectName(u"Form")
-        Form.resize(483, 350)
-        self.actSelect = QAction(Form)
+class Ui_MyWindow(object):
+    def setupUi(self, MyWindow):
+        if not MyWindow.objectName():
+            MyWindow.setObjectName(u"MyWindow")
+        MyWindow.resize(483, 350)
+        self.actSelect = QAction(MyWindow)
         self.actSelect.setObjectName(u"actSelect")
         icon = QIcon()
         icon.addFile(u":/icons/icons/open1.png", QSize(), QIcon.Normal, QIcon.Off)
         self.actSelect.setIcon(icon)
-        self.gridLayout_3 = QGridLayout(Form)
+        self.gridLayout_3 = QGridLayout(MyWindow)
         self.gridLayout_3.setObjectName(u"gridLayout_3")
-        self.groupBox = QGroupBox(Form)
+        self.groupBox = QGroupBox(MyWindow)
         self.groupBox.setObjectName(u"groupBox")
         self.gridLayout = QGridLayout(self.groupBox)
         self.gridLayout.setObjectName(u"gridLayout")
@@ -43,7 +43,7 @@ class Ui_Form(object):
 
         self.gridLayout_3.addWidget(self.groupBox, 0, 0, 1, 1)
 
-        self.groupBox_2 = QGroupBox(Form)
+        self.groupBox_2 = QGroupBox(MyWindow)
         self.groupBox_2.setObjectName(u"groupBox_2")
         self.gridLayout_2 = QGridLayout(self.groupBox_2)
         self.gridLayout_2.setObjectName(u"gridLayout_2")
@@ -71,12 +71,12 @@ class Ui_Form(object):
 
         self.horizontalLayout = QHBoxLayout()
         self.horizontalLayout.setObjectName(u"horizontalLayout")
-        self.tbtnDo = QToolButton(Form)
+        self.tbtnDo = QToolButton(MyWindow)
         self.tbtnDo.setObjectName(u"tbtnDo")
 
         self.horizontalLayout.addWidget(self.tbtnDo)
 
-        self.tbtnClose = QToolButton(Form)
+        self.tbtnClose = QToolButton(MyWindow)
         self.tbtnClose.setObjectName(u"tbtnClose")
 
         self.horizontalLayout.addWidget(self.tbtnClose)
@@ -88,7 +88,7 @@ class Ui_Form(object):
 
         self.gridLayout_3.addItem(self.verticalSpacer_2, 4, 0, 1, 1)
 
-        self.pBar = QProgressBar(Form)
+        self.pBar = QProgressBar(MyWindow)
         self.pBar.setObjectName(u"pBar")
         self.pBar.setValue(0)
         self.pBar.setTextVisible(False)
@@ -96,27 +96,28 @@ class Ui_Form(object):
         self.gridLayout_3.addWidget(self.pBar, 5, 0, 1, 1)
 
 
-        self.retranslateUi(Form)
+        self.retranslateUi(MyWindow)
+        self.tbtnClose.clicked.connect(MyWindow.close)
 
-        QMetaObject.connectSlotsByName(Form)
+        QMetaObject.connectSlotsByName(MyWindow)
     # setupUi
 
-    def retranslateUi(self, Form):
-        Form.setWindowTitle(QCoreApplication.translate("Form", u"Form", None))
-        self.actSelect.setText(QCoreApplication.translate("Form", u"\u9009\u62e9\u76ee\u5f55", None))
+    def retranslateUi(self, MyWindow):
+        MyWindow.setWindowTitle(QCoreApplication.translate("MyWindow", u"CSV Deal", None))
+        self.actSelect.setText(QCoreApplication.translate("MyWindow", u"\u9009\u62e9\u76ee\u5f55", None))
 #if QT_CONFIG(tooltip)
-        self.actSelect.setToolTip(QCoreApplication.translate("Form", u"\u9009\u62e9\u76ee\u5f55", None))
+        self.actSelect.setToolTip(QCoreApplication.translate("MyWindow", u"\u9009\u62e9\u76ee\u5f55", None))
 #endif // QT_CONFIG(tooltip)
 #if QT_CONFIG(shortcut)
-        self.actSelect.setShortcut(QCoreApplication.translate("Form", u"Ctrl+O", None))
+        self.actSelect.setShortcut(QCoreApplication.translate("MyWindow", u"Ctrl+O", None))
 #endif // QT_CONFIG(shortcut)
-        self.groupBox.setTitle(QCoreApplication.translate("Form", u"\u9009\u62e9\u76ee\u5f55", None))
-        self.tbtnSelect.setText(QCoreApplication.translate("Form", u"\u9009\u62e9", None))
-        self.groupBox_2.setTitle(QCoreApplication.translate("Form", u"\u7c7b\u578b\u9009\u62e9", None))
-        self.rbtnCSV2MAP.setText(QCoreApplication.translate("Form", u"CSV\u8f6cTXTMAP", None))
-        self.rbtnMAP2TXT.setText(QCoreApplication.translate("Form", u"\u5ba2\u4f9bMAP\u8f6cTXT", None))
-        self.rbtnCSV2TIME.setText(QCoreApplication.translate("Form", u"TIMELOG\u8f6cMAP/TIME", None))
-        self.tbtnDo.setText(QCoreApplication.translate("Form", u"\u6267\u884c", None))
-        self.tbtnClose.setText(QCoreApplication.translate("Form", u"\u9000\u51fa", None))
+        self.groupBox.setTitle(QCoreApplication.translate("MyWindow", u"\u9009\u62e9\u76ee\u5f55", None))
+        self.tbtnSelect.setText(QCoreApplication.translate("MyWindow", u"\u9009\u62e9", None))
+        self.groupBox_2.setTitle(QCoreApplication.translate("MyWindow", u"\u7c7b\u578b\u9009\u62e9", None))
+        self.rbtnCSV2MAP.setText(QCoreApplication.translate("MyWindow", u"CSV\u8f6cTXTMAP", None))
+        self.rbtnMAP2TXT.setText(QCoreApplication.translate("MyWindow", u"\u5ba2\u4f9bMAP\u8f6cTXT", None))
+        self.rbtnCSV2TIME.setText(QCoreApplication.translate("MyWindow", u"TIMELOG\u8f6cMAP/TIME", None))
+        self.tbtnDo.setText(QCoreApplication.translate("MyWindow", u"\u6267\u884c", None))
+        self.tbtnClose.setText(QCoreApplication.translate("MyWindow", u"\u9000\u51fa", None))
     # retranslateUi
 
--- main.py
+++ main.py
@@ -1,9 +1,14 @@
-from PySide2.QtWidgets import QApplication, QWidget,QFileDialog,QMessageBox
-from PySide2.QtCore import QStandardPaths,QDir
-from Ui_CSVDeal import Ui_Form
-class MyWindow(QWidget,Ui_Form):
+from PySide2.QtWidgets import QApplication, QWidget, QFileDialog, QMessageBox
+from PySide2.QtCore import QStandardPaths, QDir
+from Ui_CSVDeal import Ui_MyWindow
+import re
+import csv
+
+
+class MyWindow(QWidget, Ui_MyWindow):
     desktopPath = QStandardPaths.writableLocation(QStandardPaths.DesktopLocation)
     fileList = []
+
     def __init__(self):
         super().__init__()
         self.setupUi(self)
@@ -11,14 +16,66 @@ class MyWindow(QWidget,Ui_Form):
         self.tbtnSelect.setDefaultAction(self.actSelect)
         self.tbtnSelect.clicked.connect(self.selectDir)
         self.tbtnDo.clicked.connect(self.convertCSV2MAP)
+        self.tbtnClose.clicked.connect(self.close)
+
     def selectDir(self):
-        dir = QFileDialog.getExistingDirectory(self, "选择目录",MyWindow.desktopPath)
+        dir = QFileDialog.getExistingDirectory(self, "选择目录", MyWindow.desktopPath)
         if dir:
-            self.editDir.setText(str(dir).replace('/', '\\'))
+            self.editDir.setText(str(dir).replace("/", "\\"))
             self.pBar.setEnabled(True)
-            return 
+            return
         MessageBox = QMessageBox()
-        MessageBox.critical(self, "警告", "选择的目录路径为空,请重新选择!!!") 
+        MessageBox.critical(self, "警告", "选择的目录路径为空,请重新选择!!!")
+
+    def timeLogDeal(self, fileName):
+        re_csvHeader = re.compile(
+            r"^TotalNum,DieNum,StartTime,TestTime\(S\),IntervalTime\(S\),TotalTime\(min\),(SITE,BIN,X,Y,...)?$"
+        )
+        # 59,5,2024/7/30 10:48:13,4.515,0.451000,1.21,11,13,84,18,12,12,84,19,13,12,84,20,14,12,84,21,15,12,84,22,
+        re_csvBody = re.compile(
+            r"(?P<TotalNum>^\d+),(?P<dieNum>\d{,2}),\d{4}/\d{,2}/\d{,2} \d{,2}:\d{,2}:\d{,2},[0-9.]+,[0-9.]+,[0-9.]+,(?P<dieData>(?:\d+,\d+,\d+,\d+,)+)$"
+        )
+        re_dieData = re.compile(r"\d+,\d+,\d+,\d+,")
+        dieCnt = 0
+        totalNum = 0
+        dieNum = 0
+        dieDataCount = 0
+        file = self.editDir.text() + "\\" + fileName
+        print(file)
+        # 以txt文本模式打开file，并校验文件(使用正则)
+        with open(file, "r") as fp:
+            for cnt, line in enumerate(fp, start=1):
+                print(line)
+                if cnt == 1:
+                    if not re_csvHeader.search(line):
+                        print(f"{file}头部格式不正确")
+                        return
+                    continue
+                match = re_csvBody.search(line)
+                if match:
+                    totalNum = int(match.group("TotalNum"))
+                    dieNum = int(match.group("dieNum"))
+                    dieCnt += dieNum
+                    dieDataCount = len(re.findall(re_dieData, match.group("dieData")))
+                    if totalNum != dieCnt:
+                        print(
+                            f"{file}第{cnt}行的TotalNum与每行DieNum相加数不匹配，totalNum={totalNum}, dieCnt={dieCnt}, dieCntFront={dieCnt-dieNum}, dieNum={dieNum}"
+                        )
+                        return
+                    if dieNum != dieDataCount:
+                        print(f"{file}第{cnt}行的DieNum与DieDataCount不匹配")
+                        return
+                else:
+                    print(f"{file}第{cnt}行格式不正确")
+                    return
+
+        # 读取csv文件
+        with open(file, "r", newline="", encoding="utf-8") as csvFile:
+            csvReader = csv.reader(csvFile)
+            for row in csvReader:
+                print(row)
+        pass
+
     def convertCSV2MAP(self):
         dir = self.editDir.text()
         if not dir:
@@ -28,18 +85,15 @@ class MyWindow(QWidget,Ui_Form):
         self.fileList = QDir(dir).entryList(QDir.Files, QDir.Name)
         self.pBar.setRange(0, len(self.fileList))
         self.pBar.setValue(0)
-        strTmp =""
-        for file in self.fileList:
-            strTmp = str(file).upper()
-            if strTmp.endswith('.CSV'):
-                self.pBar.setValue(self.fileList.index(file))
-                print(file)
-        print("*"*20)
+        csvFiles = [file for file in self.fileList if file.upper().endswith(".CSV")]
+        for file in csvFiles:
+            print(file)  # 开始处理csv文件
+            self.timeLogDeal(file)
+            self.pBar.setValue(csvFiles.index(file) + 1)
+
 
 if __name__ == "__main__":
     app = QApplication([])
     window = MyWindow()
     window.show()
     app.exec_()
-
-
